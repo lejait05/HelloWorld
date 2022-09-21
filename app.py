a39a668c520a9781734b177c50b789e386c94221
lineisha gibson <lineishagibson@gmail.com>
2022-09-20 22:11:02 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+
+
+# birth_year = input("Enter your birth year: ")
+# age = 2022 - int(birth_year)
+# print(age)
+
+
+# name = input("What is your name?")
+# print("Hello ")
+
+
+# age = 25
+# price = 19.95
+# first_name = 'Charm'
+# is_online = False
+# print(age)
